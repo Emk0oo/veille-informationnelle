a08d9bf26c9fbcb06358a32ebe421b26529166f6
Javor Emir <emir.j2002@gmail.com>
2024-12-06 09:48:42 +0100

    add routing configuration for login and home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent }, // Page par défaut (login)
+  { path: 'home', component: HomeComponent }, // Page d'accueil après login
+  { path: '**', redirectTo: '' } // Redirection pour les routes non trouvées
+];
